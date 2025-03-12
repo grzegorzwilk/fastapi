9096a119ee39ffde26d4fcc4063f8b7473c0bff2
grzegorzwilk <95700863+grzegorzwilk@users.noreply.github.com>
2025-03-12 23:39:16 +0100

    main.py updated - lpx converter added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-
+import subprocess as sub 
+import os
 app = FastAPI()
 
 @app.get("/")
@@ -12,3 +13,10 @@ items = []
 def make_item(item: str):
     items.append(item)
     return items
+# call lpx
+@app.get("/lpx")
+def run_lpx():
+    lp=os.path.dirname(__file__)
+    path=os.path.join(lp,'WIN','LP_XMLConverter.exe')
+    out = sub.check_output([path])
+    return {"lpx": out}
