e84b15670098ccfa92df2c52bee94ea6a0e90f3a
grzegorzwilk <95700863+grzegorzwilk@users.noreply.github.com>
2025-03-12 23:49:42 +0100

    help endpoint added
    prints help

--- main.py
+++ main.py
@@ -7,16 +7,22 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-items = []
+# items = []
 
-@app.post("/items/")
-def make_item(item: str):
-    items.append(item)
-    return items
+# @app.post("/items/")
+# def make_item(item: str):
+#     items.append(item)
+#     return items
 # call lpx
 @app.get("/lpx")
 def run_lpx():
     lp=os.path.dirname(__file__)
     path=os.path.join(lp,'WIN','LP_XMLConverter.exe')
     out = sub.check_output([path])
-    return {"lpx": out}
+    return {"result": out}
+@app.get("/help")
+def run_lpx():
+    lp=os.path.dirname(__file__)
+    path=os.path.join(lp,'WIN','LP_XMLConverter.exe','help')
+    out = sub.check_output([path])
+    return {"result": out}
