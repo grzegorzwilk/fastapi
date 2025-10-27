20bba2c35f4934666bf771cd9cd6897e8739e959
grzegorzwilk <95700863+grzegorzwilk@users.noreply.github.com>
2025-10-27 15:09:20 +0100

    test5

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 import subprocess as sub 
 import os
 
+# lp=os.path.basename(__file__)
 lp=os.path.dirname(__file__)
+lp=os.path.dirname(lp)
 path=os.path.join(lp,'LP','LP_XMLConverter.exe')
-# o = sub.check_output(['wine',path])
-# print (o)
+# print (path)
+# # o = sub.check_output(['wine',path])
+# # print (o)
 
 import subprocess
 
